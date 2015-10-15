e008aaf9e1133f52d31205cc390e419d815b0280
pedroviniv <pfernandesvasconcelos@gmail.com>
2015-10-14 23:09:32 -0300

    Create Inserts.sql
    Created some Person's Inserts!

---
+++ Parte 1/Inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO PESSOA VALUES('11111111111','111111111','Pedro','M','26/12/1996','Paraiba','Cajazeiras','Jardim Oasis','Jose Dantas Nobre',371);
+INSERT INTO PESSOA VALUES('22222222222','222222222','João','M','25/07/1998','Paraiba','Cajazeiras','Pôr do Sol','Padre Rolim',222);
+INSERT INTO PESSOA VALUES('33333333333','333333333','Francisco','M','22/05/1983','Paraiba','João Pessoa','Bancários','Serafim Bernardo',415);
+INSERT INTO PESSOA VALUES('44444444444','444444444','Antônia','F','12/02/1982','Paraiba','João Pessoa','Juazeirinho','Cruzeiro Olinda',231);
+INSERT INTO PESSOA VALUES('55555555555','555555555','Maria','F','08/05/1968','Ceará','Fortaleza','Cormecial','Principal',1078);
+INSERT INTO PESSOA VALUES('66666666666','666666666','Lucas','M','14/09/1991','Rio Grande do Norte','Natal','Lagoa Nova','',231);
+INSERT INTO PESSOA VALUES('77777777777','777777777','Fernanda','F','02/02/1992','Paraíba','João Pessoa','Bessa','Avenida Presidente Afonso Pena',791);
