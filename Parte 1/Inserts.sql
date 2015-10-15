3df5a3dd96fbf1d93ba2c06b5423a7ee8e9851f3
pedroviniv <pfernandesvasconcelos@gmail.com>
2015-10-15 18:06:45 -0300

    Added new Person

--- Parte 1/Inserts.sql
+++ Parte 1/Inserts.sql
@@ -4,7 +4,7 @@ INSERT INTO PESSOA VALUES('22222222222','222222222','João','M','25/07/1998','Pa
 INSERT INTO PESSOA VALUES('33333333333','333333333','Francisco','M','22/05/1983','Paraiba','João Pessoa','Bancários','Serafim Bernardo',415);
 INSERT INTO PESSOA VALUES('44444444444','444444444','Antônia','F','12/02/1982','Paraiba','João Pessoa','Juazeirinho','Cruzeiro Olinda',231);
 INSERT INTO PESSOA VALUES('55555555555','555555555','Maria','F','08/05/1968','Ceará','Fortaleza','Cormecial','Av. Principal',1078);
-INSERT INTO PESSOA VALUES('66666666666','666666666','Lucas','M','14/09/1991','Rio Grande do Norte','Natal','Lagoa Nova','Av. Jardim Oásis',133);
+INSERT INTO PESSOA VALUES('66666666666','666666666','Lucas','M','14/09/1991','Rio Grande do Norte','Natal','Lagoa Nova','	',133);
 INSERT INTO PESSOA VALUES('77777777777','777777777','Fernanda','F','02/02/1992','Paraíba','João Pessoa','Bessa','Avenida Presidente Afonso Pena',791);
 INSERT INTO PESSOA VALUES('88888888888','123456789','Natarajan','M','28/03/1984','Paraíba','Sousa','Centro','Rua Cel José Gomes de Sá',7);
 INSERT INTO PESSOA VALUES('99999999999','888888888','Victor','M','08/05/1993','Paraíba','Cajazeiras','Centro','Rua Cel Antonio Silva',623);
@@ -14,6 +14,7 @@ INSERT INTO PESSOA VALUES('14141414141','141414141','José','M','20/02/1991','Pe
 INSERT INTO PESSOA VALUES('15151515151','151515151','Carlos','M','02/04/1985','Alagoas','Maceió','Santos','Av. Manel de Sousa',922);
 INSERT INTO PESSOA VALUES('16161616161','161616161','Augusto','M','25/02/1996','Rio Grande do Norte','Caicó','Centro','Rua José Nilson de sá',406);
 INSERT INTO PESSOA VALUES('17171717171','171717171','Caroline','F','23/02/1995','Rio Grande do Norte','Natal','Lagoa Nova','Av. Jardim Oásis',154);
+INSERT INTO PESSOA VALUES('18181818181','181818181','Nicole','F','05/09/2001','São Paulo','Santos','Orla','Av. Cel Santos de Sá',829);
 
 
 --TABLE TELEFONE_PESSOA Inserts
