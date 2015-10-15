7e86adac4ecf23ed321f60663be326fe7ebe5233
pedroviniv <pfernandesvasconcelos@gmail.com>
2015-10-15 10:10:00 -0300

    Added 7 new PESSOA's Inserts

--- Parte 1/Inserts.sql
+++ Parte 1/Inserts.sql
@@ -3,10 +3,18 @@ INSERT INTO PESSOA VALUES('11111111111','111111111','Pedro','M','26/12/1996','Pa
 INSERT INTO PESSOA VALUES('22222222222','222222222','João','M','25/07/1998','Paraiba','Cajazeiras','Pôr do Sol','Padre Rolim',222);
 INSERT INTO PESSOA VALUES('33333333333','333333333','Francisco','M','22/05/1983','Paraiba','João Pessoa','Bancários','Serafim Bernardo',415);
 INSERT INTO PESSOA VALUES('44444444444','444444444','Antônia','F','12/02/1982','Paraiba','João Pessoa','Juazeirinho','Cruzeiro Olinda',231);
-INSERT INTO PESSOA VALUES('55555555555','555555555','Maria','F','08/05/1968','Ceará','Fortaleza','Cormecial','Principal',1078);
-INSERT INTO PESSOA VALUES('66666666666','666666666','Lucas','M','14/09/1991','Rio Grande do Norte','Natal','Lagoa Nova','',231);
+INSERT INTO PESSOA VALUES('55555555555','555555555','Maria','F','08/05/1968','Ceará','Fortaleza','Cormecial','Av. Principal',1078);
+INSERT INTO PESSOA VALUES('66666666666','666666666','Lucas','M','14/09/1991','Rio Grande do Norte','Natal','Lagoa Nova','Av. Jardim Oásis',133);
 INSERT INTO PESSOA VALUES('77777777777','777777777','Fernanda','F','02/02/1992','Paraíba','João Pessoa','Bessa','Avenida Presidente Afonso Pena',791);
 INSERT INTO PESSOA VALUES('88888888888','123456789','Natarajan','M','28/03/1984','Paraíba','Sousa','Centro','Rua Cel José Gomes de Sá',7);
+INSERT INTO PESSOA VALUES('99999999999','888888888','Victor','M','08/05/1993','Paraíba','Cajazeiras','Centro','Rua Cel Antonio Silva',623);
+INSERT INTO PESSOA VALUES('12121212121','121212121','Bruna','F','15/11/2003','Paraíba','Cajazeiras','Centro','Rua Hortelina de Sousa Lira',204);
+INSERT INTO PESSOA VALUES('13131313131','131313131','Amanda','F','14/07/1996','Rio Grande do Norte','Caicó','Centro','Rua Serafim Bernardo',352);
+INSERT INTO PESSOA VALUES('14141414141','141414141','José','M','20/02/1991','Pernambuco','Olinda','Centro','Av. Cel Batista',801);
+INSERT INTO PESSOA VALUES('15151515151','151515151','Carlos','M','02/04/1985','Alagoas','Maceió','Santos','Av. Manel de Sousa',922);
+INSERT INTO PESSOA VALUES('16161616161','161616161','Augusto','M','25/02/1996','Rio Grande do Norte','Caicó','Centro','Rua José Nilson de sá',406);
+INSERT INTO PESSOA VALUES('17171717171','171717171','Caroline','F','23/02/1995','Rio Grande do Norte','Natal','Lagoa Nova','Av. Jardim Oásis',154);
+
 
 --TABLE TELEFONE_PESSOA Inserts
 INSERT INTO TELEFONE_PESSOA VALUES('11111111111','(83)99365-5545');
